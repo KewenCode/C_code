a23e16663960aedd0092a259dc8396b4c21f47c9
KewenCode <kkllgxn2000.com@163.com>
2021-10-27 18:55:05 +0800

    test_1_1/

---
+++ test_1_1/test_1_1/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	//printf("hallo word!\n");
+//	printf("%d\n",sizeof(char));
+//	printf("hallo word!\n");
+//	return 0;
+//}
+//
+////ctrl+F5
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	//scanf函数是输入函数
+	sum = a + b;
+		printf("sum=%d\n", sum);
+	return 0;
+}
+
