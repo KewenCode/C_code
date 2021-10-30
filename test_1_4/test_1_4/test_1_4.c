474e1942e1578388a6f09caa296810e5f85ef402
KewenCode <kkllgxn2000.com@163.com>
2021-10-30 14:16:44 +0800

    内置猜数字小游戏

--- test_1_4/test_1_4/test_1_4.c
+++ test_1_4/test_1_4/test_1_4.c
@@ -288,35 +288,169 @@
 //在一个有序数组中查找某个具体数字n
 //折半查找 二分查找
 
-int main()
-{
-    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-    int k = 7;//要查找的数字
-    //在arr这个有序的数组中查找k（7）的值
-    int sz = sizeof(arr) / sizeof(arr[0]);//数组的元素个数
-    int left = 0;
-    int right = sz-1;
+//int main()
+//{
+//    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//    int k = 7;//要查找的数字
+//    //在arr这个有序的数组中查找k（7）的值
+//    int sz = sizeof(arr) / sizeof(arr[0]);//数组的元素个数
+//    int left = 0;
+//    int right = sz-1;
+//
+//    while (left<=right)
+//    {
+//        int mid = (right + left) / 2;
+//        if (arr[mid] < k)
+//        {
+//            left = mid + 1;
+//        }
+//        else if (arr[mid] > k)
+//        {
+//            right = mid - 1;
+//        }
+//        else
+//        {
+//            printf("找到了，下标是：%d\n", mid);
+//            break;
+//        }
+//    }
+//    if (left > right)
+//    {
+//        printf("找不到\n");
+//    }
+//    return 0;
+//}
 
-    while (left<=right)
+//welcome to nanjing!!!
+//w###################!
+//we#################!!
+//...
+
+//#include<string.h>
+//#include<windows.h>
+//int main()
+//{
+//    char arr1[] = "welcome to nanjing!!!";
+//    char arr2[] = "#####################";
+//    int left = 0;
+//    int right = strlen(arr1) - 1;
+//
+//    while (left <= right)
+//    {
+//        arr2[left] = arr1[left];
+//        arr2[right] = arr1[right];
+//        printf("%s\n", arr2);
+//        Sleep(1000);//睡眠1秒 windows.h
+//        system("cls");//清空屏幕
+//        left++;
+//        right--;
+//    }
+//    printf("%s\n", arr1);
+//    return 0;
+//}
+
+
+//最多登陆三次
+ 
+//#include<string.h>
+//int main()
+//{
+//    int i = 0;
+//    char password[20] = { 0 };
+//    for (i = 0;i < 3;i++)
+//    {
+//        printf("请输入密码：");
+//        scanf("%s", password);
+//        //两个字符串比较，不能使用==，应该使用strcmp
+//        if (strcmp(password, "123456")==0)
+//        {
+//            printf("登陆成功\n");
+//            break;
+//        }
+//        else
+//        {
+//            printf("密码错误，重新输入\n");
+//        }
+//    }
+//    if (i == 3)
+//    {
+//        printf("三次密码均错误，退出程序\n");
+//    }
+//
+//    return 0;
+//}
+
+
+//猜数字游戏
+//1、自动产生一个1-100之间的随机数
+//2、猜数字
+//   a.猜对了
+//   b.猜错了
+// 3、一直玩
+//
+
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+void menu()
+{
+    printf("******************\n");
+    printf("*** 猜数字游戏 ***\n");
+    printf("***  1.  play  ***\n");
+    printf("***  0.  exit  ***\n");
+    printf("******************\n");
+}
+void game()
+{
+    //猜数字游戏实现
+    //1.生成随机数字
+    //rand函数返回了一个0-32767之间数字
+    //时间 - 时间戳
+    int ret = rand()%100+1;//%100的余数是0-99，然后+1，范围就是1-100
+    //2.猜数字
+    int guess = 0;
+    while (1)
     {
-        int mid = (right + left) / 2;
-        if (arr[mid] < k)
+        printf("请猜数字(0-100)：");
+        scanf("%s", &guess);
+        if (guess < ret)
         {
-            left = mid + 1;
+            printf("猜小了\n");
         }
-        else if (arr[mid] > k)
+        else if(guess > ret)
         {
-            right = mid - 1;
+            printf("猜大了\n");
         }
-        else
+        else 
         {
-            printf("找到了，下标是：%d\n", mid);
+            printf("恭喜你，猜对了\n");
             break;
         }
     }
-    if (left > right)
+}
+int main()
+{
+    int input = 0;
+    int input1 = 0;
+    srand((unsigned int)time(NULL));
+    do
     {
-        printf("找不到\n");
-    }
+        menu();//打印菜单
+        printf("请选择：");
+        scanf("%d", &input);
+        switch (input)
+        {
+        case 1:
+            //printf("猜数字：\n");
+            game();
+            break;
+        case 0:
+            printf("退出游戏\n");
+            break;
+        default:
+            printf("选择错误，重新选择\n");
+            break;
+        }
+    } while (input);
     return 0;
 }
