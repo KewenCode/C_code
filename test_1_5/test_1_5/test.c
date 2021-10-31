bfda73d15d5070ac27b0f49a22804902da07c42e
KewenCode <kkllgxn2000.com@163.com>
2021-11-01 00:02:55 +0800

    库函数

--- test_1_5/test_1_5/test.c
+++ test_1_5/test_1_5/test.c
@@ -1,2 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//库函数 - 大量使用的函数
+
+#include<string.h>
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "hello world";
+//	strcpy(arr1, arr2);
+//	printf("%s",arr1); //打印arr1这个字符串%s - 以字符串形式打印
+//
+//	return 0;
+//}
+
+int main()
+{
+	char arr[] = "hallo world";
+	memset(arr, 'x', 5);
+	printf("%s", arr);
+	return 0;
+}
