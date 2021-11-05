3ec131615476c2e37a20ce0eb9be66f03e1fbaa9
KewenCode <kkllgxn2000.com@163.com>
2021-11-06 00:52:13 +0800

    斐波那契数列、汉诺塔、青蛙跳台阶

--- test_1_5/test_1_5/test.c
+++ test_1_5/test_1_5/test.c
@@ -268,20 +268,103 @@
 // 12/10  =1%10   =1
 // 1/10   =0
 
-void print(unsigned int n)
-{
-	if (n > 9)
-	{
-		print(n / 10);
-	}
-	printf("%d ", n % 10);
-}
+//void print(unsigned int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%u", &num);//1234
+//	//递归 - 函数自己调用自己
+//	print(num);//print函数可以打印参数部分数字的每一位
+//	return 0;
+//}
 
+
+//编写函数不允许创建临时变量，求字符串长度。
+//int my_strlen(char* str)
+//{
+//	//////int count = 0;
+//	//////while (*str != '\0')
+//	//////{
+//	//////	count++;
+//	//////	str++;
+//	//////}
+//	//////return count;
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	//['b'] ['i'] ['t'] ['\0']
+//	//模拟实现一个strlen函数
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+
+//阶乘
+//int Fac(int n);
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac(n - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fac(n);
+//	printf("%d", ret);
+//	return 0;
+//}
+//有一些功能，可以用迭代的方法，也可以用递归的方法
+
+//求第n个斐波那契数
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fib(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while(n>2)
+	{
+				c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
 int main()
 {
-	unsigned int num = 0;
-	scanf("%u", &num);//1234
-	//递归 - 函数自己调用自己
-	print(num);//print函数可以打印参数部分数字的每一位
+	int n = 0;
+	scanf("%d", &n);
+	int ret = Fib(n);
+	printf("%d\n", ret);
 	return 0;
 }
