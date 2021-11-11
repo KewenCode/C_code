e14bd3b122c9723a9959a5eb0c33c8f5522cd7bc
KewenCode <kkllgxn2000.com@163.com>
2021-11-11 22:03:03 +0800

    冒泡函数

--- test_1_6/test_1_6/test.c
+++ test_1_6/test_1_6/test.c
@@ -72,3 +72,62 @@
 //	return 0;
 //}
 
+
+//冒泡排序
+//void bubble_sort(int arr[],int sz)
+//{
+//	//确定趟数
+//	int i = 0;
+//	int flag = 1;//与0比较，消除多余的无效循环
+// 
+//	for (i = 0;i < sz - 1;i++)
+//	{
+//		//一趟冒泡排序的过程
+//		int j = 0;
+//		for (j = 0;j < sz-1-i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				//交换
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	//排序位升序 - 冒泡排序
+//	//计算数组元素个数
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr,sz);
+//
+//	return 0;
+//}
+
+
+//数组名是什么
+//数组名是数组首元素的地址
+// 但是有两个意外
+// 1. sizeof（数组名） - 数组名表示整个数组 - 计算的是整个数组的大小单位是字节
+// 2. &数组名 - 数组名表示整个数组 - 取出的是整个数组的地址
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", &arr);//&arr取出的是数组的地址
+//
+//	//printf("%p\n", &arr[0]);
+//	//printf("%p\n", arr);//数组名是首元素的地址
+//
+//	int sz = sizeof(arr);//数组名表示整个数组
+//
+//	return 0;
+//}
