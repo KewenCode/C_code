26a5ca9218b0f6237f6d2194b093b2897aba1fdd
KewenCode <kkllgxn2000.com@163.com>
2021-11-20 19:04:09 +0800

    指针优先级

--- test_1_7/test_1_7/test.c
+++ test_1_7/test_1_7/test.c
@@ -312,3 +312,21 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int i = 1;
+//	int ret = (++i) + (++i) + (++i);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+int main()
+{
+	char str[] = "hello bit";
+	//[hello bit\0] - 0是转义字符，只有一个字符
+	printf("%d %d\n", sizeof(str), strlen(str));
+	//10 9
+	//strlen - 函数 - 求字符串长度的，找\0之前出现的字符个数
+	//sizeof - 操作符 - 计算变量/类型所占的内存大小，单位是字节
+	return 0;
+}
