4ba4030205be9930b3f9f0c6cb8f8ac22a21d0b8
KewenCode <kkllgxn2000.com@163.com>
2021-11-21 16:28:13 +0800

    数组复习

--- test_1_7/test_1_7/test.c
+++ test_1_7/test_1_7/test.c
@@ -320,13 +320,74 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	char str[] = "hello bit";
+//	//[hello bit\0] - 0是转义字符，只有一个字符
+//	printf("%d %d\n", sizeof(str), strlen(str));
+//	//10 9
+//	//strlen - 函数 - 求字符串长度的，找\0之前出现的字符个数
+//	//sizeof - 操作符 - 计算变量/类型所占的内存大小，单位是字节
+//	return 0;
+//}
+
+
+//void init(int arr[ ], int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//void print(int arr[ ], int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void reverse(int arr[ ], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	reverse(arr, sz);
+//	print(arr, sz);
+//	init(arr, sz);
+//	print(arr, sz);
+//	return 0;
+//}
+
 int main()
 {
-	char str[] = "hello bit";
-	//[hello bit\0] - 0是转义字符，只有一个字符
-	printf("%d %d\n", sizeof(str), strlen(str));
-	//10 9
-	//strlen - 函数 - 求字符串长度的，找\0之前出现的字符个数
-	//sizeof - 操作符 - 计算变量/类型所占的内存大小，单位是字节
+	int arr1[] = { 1,3,5,7,9 };
+	int arr2[] = { 2,4,6,8,10 };
+	
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+
+	for (i = 0;i < sz;i++)
+	{
+		int tmp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = tmp;
+	}
 	return 0;
+
 }
