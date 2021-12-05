216b087e5ae744ec8b47ba7ca2d2b0e59382fb41
KewenCode <kkllgxn2000.com@163.com>
2021-12-05 08:17:12 +0800

    指针初阶作业

--- test_2_2/test_2_2/test.c
+++ test_2_2/test_2_2/test.c
@@ -85,38 +85,143 @@
 //}
 
 
-void print(int arr[3][5], int r, int c)
+//void print(int arr[3][5], int r, int c)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0;i < r;i++)
+//	{
+//		for (j = 0;j < c;j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+////p是数组指针
+//void print2(int(*p)[5], int r, int c)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0;i < r;i++)
+//	{
+//		for (j = 0;j < c.j++)
+//		{
+//			printf("%d ", *(*(p + i)+j));
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	//int a[5];
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//	/*print(arr, 3, 5);*/
+//	print2(arr, 3, 5);//arr数组名，表示数组首元素地址
+//	return 0;
+//}
+
+
+//int i;
+//int main()
+//{
+//	i--;
+//	//sizeof这个操作符，返回的结果是unsigned int
+//	if (i > sizeof(i))
+//	{
+//		printf(">")
+//	}
+//	else
+//	{
+//		printf("<")
+//	}
+//	return 0;
+//}
+
+//求sn=a+aa+aaa+aaaa+aaaaa的前五项之和，其中a是一个数字
+//int main()
+//{
+//	//计算由a组成的前n项之和
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d %d", &a, &n);
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 0;
+//	for (i = 0;i < n;i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//打印arr数组
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5 };
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+//求1-100000之间所有的水仙花数
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0;i <= 100000;i++)
+//	{
+//		int n = 1;
+//		int tmp = i;
+//		while (tmp / 10)
+//		{
+//			n++;
+//			tmp = tmp / 10;
+//		}
+//		tmp = i;
+//		int sum = 0;
+//		while (tmp)
+//		{
+//			//pow是用来求次方
+//			sum += pow(tmp % 10, n);
+//			tmp = tmp / 10;
+//		}
+//		if (sum == i)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+#include <assert.h>
+void reverse(char* str)
 {
-	int i = 0;
-	int j = 0;
-	for (i = 0;i < r;i++)
+	assert(str != NULL);
+	int len = strlen(str);
+	char* left = str;
+	char* right = (str + len - 1);
+	while (left < right)
 	{
-		for (j = 0;j < c;j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-}
-//p是数组指针
-void print2(int(*p)[5], int r, int c)
-{
-	int i = 0;
-	int j = 0;
-	for (i = 0;i < r;i++)
-	{
-		for (j = 0;j < c.j++)
-		{
-			printf("%d ", *(*(p + i)+j));
-		}
-		printf("\n");
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
 	}
 }
 int main()
 {
-	//int a[5];
-	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
-	/*print(arr, 3, 5);*/
-	print2(arr, 3, 5);//arr数组名，表示数组首元素地址
+	char arr[] = "abcdef";
+	reverse(arr);
+	printf("%s\n", arr);
 	return 0;
 }
