032117f23130f8c3d1231d19f9c27b8d43a344a6
KewenCode <kkllgxn2000.com@163.com>
2021-12-07 08:53:56 +0800

    更新

--- test_2_2/test_2_2/test.c
+++ test_2_2/test_2_2/test.c
@@ -286,44 +286,102 @@
 //}
 
 //输入数组，调整奇数位
-void print(int arr[], int sz)
-{
-	int i = 0;
-	for (i = 0;i <= sz;i++)
-	{
-		printf("%d", arr[i]);
-	}
-	printf("\n");
-}
-void move(int arr[], int sz)
-{
-	int left = 0;
-	int right = sz - 1;
-	while (left < right)
-	{
-		//从前方后找偶数
-		while ((left < right) && (arr[left] % 2 == 1))
-		{
-			left++;
-		}
-		//从后往前找奇数
-		while ((left < right) && (arr[right] % 2 == 0))
-		{
-			right++;
-		}
-		if (left < right)
-		{
-			int tmp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = tmp;
-		}
-	}
-}
-int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	move(arr, sz);
-	print(arr, sz);
-	return 0;
-}
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i <= sz;i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void move(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		//从前方后找偶数
+//		while ((left < right) && (arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		//从后往前找奇数
+//		while ((left < right) && (arr[right] % 2 == 0))
+//		{
+//			right++;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	move(arr, sz);
+//	print(arr, sz);
+//	return 0;
+//}
+
+//32位大端模式处理器上变量b
+//int main()
+//{
+//	unsigned int a = 0x1234;
+//	// 00 00 12 34
+//	//强制类型转换后，只取第一个地址 00
+//	unsigned char b = *(unsigned char*)&a;;
+//	return 0;
+//}
+
+//打印杨辉三角
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0;i < 10;i++)
+//	{
+//		for(j = 0;j <= i;j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (j == i)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			if (i >= 2 && j >= 1)
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//		}
+//	}
+//	for (i = 0;i < 10;i++)
+//	{
+//		for (j = 0;j <= i;j++)
+//		{
+//			printf("%3d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'A';killer <= 'D';killer++)
+//	{
+//		if ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)
+//		{
+//			printf("%c\n", killer);
+//		}
+//	}
+//	return 0;
+//}
