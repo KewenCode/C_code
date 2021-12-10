4f8549e472e12251d40902f01f8becf8aeab07fc
KewenCode <kkllgxn2000.com@163.com>
2021-12-11 07:27:40 +0800

    qsort排序

--- test_2_2/test_2_2/test.c
+++ test_2_2/test_2_2/test.c
@@ -550,104 +550,192 @@
 //	return 0;
 //}
 
-int Add(int x, int y)
-{
-	return x + y;
-}
-int Sub(int x, int y)
-{
-	return x - y;
-}
-int Mul(int x, int y)
-{
-	return x * y;
-}
-int Div(int x, int y)
-{
-	return x / y;
-}
-void menu()
-{
-	printf("*** 1.add 2.sub ***\n");
-	printf("*** 3.mul 4.dic ***\n");
-	printf("*** 0.exit ***\n");
-}
-int main()
-{
-	//计算器 - 计算整型加减乘除
-	int input = 0;
-	do {
-		menu();
-		
-		int x = 0;
-		int y = 0;
-		int ret = 0;
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//void menu()
+//{
+//	printf("*** 1.add 2.sub ***\n");
+//	printf("*** 3.mul 4.dic ***\n");
+//	printf("*** 0.exit ***\n");
+//}
+//int main()
+//{
+//	//计算器 - 计算整型加减乘除
+//	int input = 0;
+//	do {
+//		menu();
+//		
+//		int x = 0;
+//		int y = 0;
+//		int ret = 0;
+//
+//		printf("请选择\n");
+//		scanf("%d", &input);
+//
+//		if (input >= 1 && input <= 4)
+//		{
+//
+//			//pfArr是函数指针
+//			//转移表 - 《c和指针》
+//			int (*pfArr[5])(int, int) = { 0,Add,Sub,Mul,Div };//将下表对应
+//			printf("请输入两个操作数\n");
+//			scanf("%d %d", &x, &y);
+//			int ret = pfArr[input](x, y);
+//			printf("ret=%d\n", ret);
+//		}
+//		else if(input==0)
+//		{
+//			printf("退出程序\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("重新选择\n");
+//		}
+//		/*switch (input)
+//		{
+//		case 1:
+//			printf("请输入两个操作数\n");
+//			scanf("%d %d", &x, &y);
+//			ret = Add(x, y);
+//			printf("ret=%d\n", ret);
+//			break;
+//		case 2:
+//			printf("请输入两个操作数\n");
+//			scanf("%d %d", &x, &y);
+//			ret = Sub(x, y);
+//			printf("ret=%d\n", ret);
+//			break;
+//		case 3:
+//			printf("请输入两个操作数\n");
+//			scanf("%d %d", &x, &y);
+//			ret = Mul(x, y);
+//			printf("ret=%d\n", ret);
+//			break;
+//		case 4:
+//			printf("请输入两个操作数\n");
+//			scanf("%d %d", &x, &y);
+//			ret = Div(x, y);
+//			printf("ret=%d\n", ret);
+//			break;
+//		case 0:
+//			printf("退出程序\n");
+//			break;
+//		default:
+//			printf("重新选择\n");
+//			break;
+//		}*/
+//	} while (input);
+//	return 0;
+//}
+////回调函数
+//int calc(int(*pf)(int, int))
+//{
+//	int x = 0;
+//	int y = 0;
+//	printf("请输入两个操作数\n");
+//	scanf("%d %d", &x, &y);
+//	return pf(x, y);
+//}
 
-		printf("请选择\n");
-		scanf("%d", &input);
+//冒泡排序
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	//冒泡排序趟数
+//	for (i = 0;i < sz - 1;i++)
+//	{
+//		//一趟冒泡排序
+//		int j = 0;
+//		for (j = 0;j < sz - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				//交换
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//void print_arr(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	//升序
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	print_arr(arr, sz);
+//	return 0;
+//}
 
-		if (input >= 1 && input <= 4)
-		{
-
-			//pfArr是函数指针
-			//转移表 - 《c和指针》
-			int (*pfArr[5])(int, int) = { 0,Add,Sub,Mul,Div };//将下表对应
-			printf("请输入两个操作数\n");
-			scanf("%d %d", &x, &y);
-			int ret = pfArr[input](x, y);
-			printf("ret=%d\n", ret);
-		}
-		else if(input==0)
-		{
-			printf("退出程序\n");
-			break;
-		}
-		else
-		{
-			printf("重新选择\n");
-		}
-		/*switch (input)
-		{
-		case 1:
-			printf("请输入两个操作数\n");
-			scanf("%d %d", &x, &y);
-			ret = Add(x, y);
-			printf("ret=%d\n", ret);
-			break;
-		case 2:
-			printf("请输入两个操作数\n");
-			scanf("%d %d", &x, &y);
-			ret = Sub(x, y);
-			printf("ret=%d\n", ret);
-			break;
-		case 3:
-			printf("请输入两个操作数\n");
-			scanf("%d %d", &x, &y);
-			ret = Mul(x, y);
-			printf("ret=%d\n", ret);
-			break;
-		case 4:
-			printf("请输入两个操作数\n");
-			scanf("%d %d", &x, &y);
-			ret = Div(x, y);
-			printf("ret=%d\n", ret);
-			break;
-		case 0:
-			printf("退出程序\n");
-			break;
-		default:
-			printf("重新选择\n");
-			break;
-		}*/
-	} while (input);
+#include <stdlib.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
+void print_arr(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0;i < sz;i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int test2()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	print_arr(arr, sz);
 	return 0;
 }
-//回调函数
-int calc(int(*pf)(int, int))
+
+int sort_age(const void* e1, const void* e2)
 {
-	int x = 0;
-	int y = 0;
-	printf("请输入两个操作数\n");
-	scanf("%d %d", &x, &y);
-	return pf(x, y);
+	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+}
+struct Stu
+{
+	char name[20];
+	int age;
+};
+void test2()
+{
+	//qsort对结构体排序
+	struct Stu s[3] = { {"zhangsan",30} ,{"lisi",23},{"wangwu",45} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	//按年龄排序
+	qsort(s, sz, sizeof(s[0]), sort_age);
+}
+
+int main()
+{
+	//test1();
+	test2();
+	return 0;
 }
