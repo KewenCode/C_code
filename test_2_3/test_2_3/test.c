76b265abbc6342bc73f9e6cd27679b0f1377a1a4
KewenCode <kkllgxn2000.com@163.com>
2021-12-21 07:19:44 +0800

    strcmp/strncpy/strncat/strncmp实现

--- test_2_3/test_2_3/test.c
+++ test_2_3/test_2_3/test.c
@@ -36,25 +36,100 @@
 //	return 0;
 //}
 
-char* my_strcat(char* dest, const char* src)
-{
-	int* ret = dest;
-	//1.找到目标字符串中的\0
-	while (*dest)
-	{
-		dest++;
-	}
-	//2.数据追加
-	while (*dest++ = *src++)
-	{
+  //strcat不能给自己追加
+//char* my_strcat(char* dest, const char* src)
+//{
+//	int* ret = dest;
+//	//1.找到目标字符串中的\0
+//	while (*dest)
+//	{
+//		dest++;
+//	}
+//	//2.数据追加
+//	while (*dest++ = *src++)
+//	{
+//
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr[20] = "hello ";
+//	char arr2[] = "world";
+//	my_strcat(arr, arr2);//字符串追加（链接）
+//	return 0;
+//}
 
-	}
-	return ret;
-}
-int main()
-{
-	char arr[20] = "hello ";
-	char arr2[] = "world";
-	my_strcat(arr, arr2);//字符串追加（链接）
-	return 0;
-}
+  //strcmp - 字符串比较大小
+//int main()
+//{
+//	int ret = strcmp("abbb", "abc");//<0
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
+//
+//int my_strcmp(const char* s1, const char* s2)
+//{
+//	assert(s1 && s2);
+//	while (*s1 == *s2)
+//	{
+//		*s1++;
+//		*s2++;
+//	}
+//	/*return s1 - s2;*/
+//	if (*s1 > *s2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//int main()
+//{
+//	char* p = "obc";
+//	char* q = "abcdef";
+//	int ret = my_strcmp(p, q);
+//	if (ret > 0)
+//	{
+//		printf("p>q\n");
+//	}
+//	else if (ret < 0)
+//	{
+//		printf("p<q\n");
+//	}
+//	else
+//	{
+//		printf("p==q\n");
+//	}
+//	return 0;
+//}
+
+  //strncpy
+//int main()
+//{
+//	char arr1[20] = "abcdef";
+//	char arr2[] = "qwer";
+//	strncpy(arr1, arr2, 2);
+//	return 0;
+//}
+
+ //strncmp
+//int main()
+//{
+//	char arr1[20] = "hello ";
+//	char arr2[] = "world";
+//	strncat(arr1, arr2, 3);
+//	return 0;
+//}
+
+ //strncmp
+//int main()
+//{
+//	char* p = "abcdef";
+//	char* q = "ancqwert";
+//	int ret = strncmp(p, q, 3);
+//	return 0;
+//}
